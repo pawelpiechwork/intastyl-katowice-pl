86f04efe0a5a49ea348c470b52896fb10f01b83f
Paweł Piech <238474790+pawelpiechwork@users.noreply.github.com>
2025-11-26 22:06:10 +0100

    feat: add ci/cd workflow for github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const PROD_SITE = process.env.SITE_URL ?? 'http://localhost:4321';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://pawelpiechwork.github.io/intastyl-katowice-pl/',
+  base: '/intastyl-katowice-pl/',
+});
