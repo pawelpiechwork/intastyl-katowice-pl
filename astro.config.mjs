20a93f0aac004c0833031a005ef7ce559ec45bb1
Paweł Piech <238474790+pawelpiechwork@users.noreply.github.com>
2025-11-26 23:15:07 +0100

    feat: add tailwindcss to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://pawelpiechwork.github.io/intastyl-katowice-pl/",
   base: "/intastyl-katowice-pl/",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
