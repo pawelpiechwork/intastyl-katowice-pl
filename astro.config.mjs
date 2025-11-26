15e1e005ef7d88fa845ccd8cd3201676f27b4fe0
Paweł Piech <238474790+pawelpiechwork@users.noreply.github.com>
2025-11-26 23:21:27 +0100

    feat: add basic website structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+const site = import.meta.env.DEV
+  ? "http://localhost:4321"
+  : "https://pawelpiechwork.github.io/intastyl-katowice-pl/";
+
+const base = import.meta.env.DEV ? "/" : "/intastyl-katowice-pl/";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pawelpiechwork.github.io/intastyl-katowice-pl/",
-  base: "/intastyl-katowice-pl/",
+  site,
+  base,
 
   vite: {
     plugins: [tailwindcss()],
