8fc896369118479e962f3fa669935c2c83be8da0
Paweł Piech <238474790+pawelpiechwork@users.noreply.github.com>
2025-11-27 01:15:05 +0100

    build(astro): remove manual site and base configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-const site = import.meta.env.DEV
-  ? "http://localhost:4321"
-  : "https://pawelpiechwork.github.io/intastyl-katowice-pl/";
-
-const base = import.meta.env.DEV ? "/" : "/intastyl-katowice-pl/";
-
 // https://astro.build/config
 export default defineConfig({
-  site,
-  base,
-
   vite: {
     plugins: [tailwindcss()],
   },
